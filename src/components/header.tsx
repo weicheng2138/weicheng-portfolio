5cf5f03215526f077078084d8630942f9f223595
weicheng2138 <weicheng2138@gmail.com>
2024-01-09 08:19:37 +0800

    feat(home): let text-roller to be center

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,30 +1,44 @@
 import { useTranslation } from 'react-i18next';
 import { ModeToggle } from '@/components/mode-toggle';
 import { I18nToggle } from '@/components/i18n-toggle';
+import { Button } from '@/components/ui/button';
 import { Link, NavLink } from 'react-router-dom';
-import { HiCode } from 'react-icons/hi';
+import { HiCode, HiMenuAlt3 } from 'react-icons/hi';
+import useBreakpoint from '@/hooks/useBreakpoint';
 
 function Header() {
   const { t } = useTranslation();
+  const breakpoint = useBreakpoint();
+
   return (
     <header className="fixed flex h-[4.5rem] w-full justify-center">
       <div className="flex w-full max-w-5xl items-center justify-between px-2 sm:px-4">
-        <HiCode className="h-10 w-10" />
+        <Button variant="ghost" size="icon">
+          <HiCode className="h-[1.2rem] w-[1.2rem]" />
+        </Button>
 
         <section className="flex items-center gap-2">
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? 'bg-red-300 dark:bg-yellow-600' : ''
-            }
-            to="/about"
-          >
-            {t('nav.about')}
-          </NavLink>
-          <NavLink to="/projects">{t('nav.projects')}</NavLink>
-          <Link to="http://google.com">{t('nav.resume')}</Link>
-          <ModeToggle />
-          |
-          <I18nToggle />
+          {breakpoint === 'xs' ? (
+            <Button variant="ghost" size="icon">
+              <HiMenuAlt3 className="h-[1.2rem] w-[1.2rem]" />
+            </Button>
+          ) : (
+            <>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? 'bg-red-300 dark:bg-yellow-600' : ''
+                }
+                to="/about"
+              >
+                {t('nav.about')}
+              </NavLink>
+              <NavLink to="/projects">{t('nav.projects')}</NavLink>
+              <Link to="http://google.com">{t('nav.resume')}</Link>
+              <ModeToggle />
+              |
+              <I18nToggle />
+            </>
+          )}
         </section>
       </div>
     </header>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,11 +11,26 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex h-dvh items-center justify-center">
-      {/* <Typography variant="h1">HOME</Typography> */}
-      <TextRoller />
-      {/* <button onClick={() => navigate('/about')}>To About</button> */}
-    </div>
+    <>
+      <div className="flex h-dvh items-center justify-center">
+        {/* <Typography variant="h1">HOME</Typography> */}
+        <TextRoller />
+        {/* <button onClick={() => navigate('/about')}>To About</button> */}
+      </div>
+      <main>
+        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+        enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
+        exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
+        nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+        minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
+        elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
+        Lorem duis laboris cupidatat officia voluptate. Culpa proident
+        adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
+        Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
+        Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
+        et culpa duis.
+      </main>
+    </>
   );
 };
 
