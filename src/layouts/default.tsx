907e66b19dbaf139b237a72d06a0f389b60532be
weicheng2138 <weicheng2138@gmail.com>
2024-04-26 10:47:42 +0800

    feat(layout): deal with layout shifting when toggle drawer

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -3,12 +3,13 @@ import Header from '@/components/header';
 import { Outlet } from 'react-router-dom';
 import MenuDrawer from '@/components/menu-drawer';
 import useBreakpoint from '@/hooks/useBreakpoint';
-import { cn } from '@/lib/utils';
+import { cn, getScrollbarWidth } from '@/lib/utils';
 import Footer from '@/components/footer';
 
 const DefaultLayout = () => {
   const [toggleDrawer, setToggleDrawer] = useState(false);
   const breakpoint = useBreakpoint();
+  const scrollbarWidth = getScrollbarWidth();
 
   const handleDrawerClick = () => setToggleDrawer(() => !toggleDrawer);
 
@@ -17,6 +18,14 @@ const DefaultLayout = () => {
       setToggleDrawer(() => false);
     }
   }, [breakpoint]);
+
+  useEffect(() => {
+    if (toggleDrawer) {
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+    } else {
+      document.body.style.paddingRight = `0px`;
+    }
+  }, [toggleDrawer, scrollbarWidth]);
   return (
     <div className={cn('relative flex flex-col items-center scroll-smooth')}>
       <Header className="z-30" handleDrawerClick={handleDrawerClick} />
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getScrollbarWidth() {
+  // Creating invisible container
+  const outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement('div');
+  outer.appendChild(inner);
+
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+  // Removing temporary elements from the DOM
+
+  if (!outer.parentNode) {
+    throw new Error('getScrollbarWidth: outer.parentNode is null');
+  }
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+}
